a69ec9cbeb3fb65f19a90aec19f1c1a7c63bac26
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-09-22 22:05:06 +0200

    Add routes for recipe pages

--- app.py
+++ app.py
@@ -7,8 +7,22 @@ app = Flask(__name__)
 @app.route("/index")
 @app.route("/home")
 def home():
-    name_image_tuple = [("Air-Fried Korean Chicken", "chickenwings.png"),
-                        ("Karaage", "karaage.png"),
-                        ("Shoyu Chicken", "shoyuchicken.png")]
+    name_image_tuple = [("Air-Fried Korean Chicken", "chickenwings.png", "wings"),
+                        ("Karaage", "karaage.png", "karaage"),
+                        ("Shoyu Chicken", "shoyuchicken.png", "shoyuchicken")]
 
     return render_template("index.html", items=name_image_tuple)
+
+@app.route("/chickenwings")
+def wings():
+    return render_template("chickenwings.html")
+
+@app.route("/karaage")
+def karaage():
+    return render_template("karaage.html")
+
+@app.route("/shoyuchicken")
+def shoyuchicken():
+    return render_template("shoyuchicken.html")
+
+
